1b57741be18386a3700f292d59ef9d3026655222
Milosz <sobiczewski.milosz@gmail.com>
2020-05-07 11:23:57 +0200

    memory active True

--- src/reminder/models.py
+++ src/reminder/models.py
@@ -6,5 +6,5 @@ class Memory(models.Model):
     text = models.TextField()
     day = models.PositiveIntegerField()
     deadline = models.CharField(max_length=100)
-    active = models.BooleanField()
+    active = models.BooleanField(default=True)
     date = models.DateField(null=True, blank=True)
